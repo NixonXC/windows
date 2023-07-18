d892dc9d286fc1dc6a93fc56931d6eea7db01977
Dev-Atharv <invasionyt19@gmail.com>
2023-07-18 20:29:12 +0530

    Upload Main System

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function updateClock() {
+    var timeElement = document.querySelector('.time');
+  
+    if (timeElement) {
+      var currentTime = new Date();
+      var hours = currentTime.getHours().toString().padStart(2, '0');
+      var minutes = currentTime.getMinutes().toString().padStart(2, '0');
+      var amOrPm = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12;
+      var timeString = hours + ':' + minutes + ' ' + amOrPm;
+      var dateString = currentTime.getDate() + '/' + (currentTime.getMonth() + 1) + '/' + currentTime.getFullYear();
+  
+      timeElement.innerHTML = '<p>' + timeString + '</p>' + '<p>' + dateString + '</p>';
+    }
+  }
+  
+  // Call the updateClock function every second to keep the clock updated
+  setInterval(updateClock, 1000);
